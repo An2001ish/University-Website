bbd1e8ef4b81061be1c63e988f713403e4907372
Anish Baraily <An2001.baraily@gmail.com>
2024-03-08 16:45:49 +0545

    creating gallery

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Header from './Components/Header/Header'
+import Programs from './Components/Programs/Programs'
+import Title from './Components/Title/Title'
+import About from './Components/About/About'
+import Campus from './Components/Campus/Campus'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Header/>
+      <div className="container">
+      <Title subTitle = 'Our PROGRAM' title = 'What We Offer'/>
+      <Programs/>
+      <About/>
+      <Title subTitle = 'Gallery' title = 'Campus Photos'/>
+      <Campus/>
+      </div>
     </div>
   )
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
+import {useState} from 'react'
 
 const Navbar = () => {
+
+  const [sticky,setSticky] = useState(false)
+
+  useEffect(()=>{
+    window.addEventListener('scroll',()=>{
+      window.scrollY > 500 ? setSticky(true):setSticky(false);
+    })
+  },[])
+
   return (
-    <nav className='container'>
+    <nav className={`container ${sticky? 'dark-nav':''}`}>
       <img src={logo} alt="" className='logo'/>
       <ul>
         <li>Home</li>
